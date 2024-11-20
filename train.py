2beef6343b98661db65fd10993532a921bc1023b
rohanbanerjee <banerjee.rohan98@gmail.com>
2024-11-20 17:57:32 +0000

    added logging and DDP

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 import torch.nn as nn
 from torchvision import datasets, transforms
+from torch.cuda.amp import autocast
+import torch.distributed as dist
 
 from data.dataset import ECGDataset
 from models.vqvae import VQVAE, SimpleVQAutoEncoder, ResVQAutoEncoder
@@ -10,6 +12,9 @@ import os
 from tqdm.auto import trange
 import wandb
 import yaml
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 """
 Author: Rohan Banerjee
@@ -49,16 +54,20 @@ def load_checkpoint(model, optimizer, checkpoint_dir='checkpoints/'):
     return start_epoch
 
 def evaluate(model, data_loader):
+    logging.debug("Starting evaluation...")
     model.eval()
     total_loss = 0
     with torch.no_grad():
-        for batch in data_loader:
+        for i, batch in enumerate(data_loader):
+            logging.debug(f"Evaluating batch {i+1}/{len(data_loader)}")
             signals = batch['signal'].float().to(device)
             signals = signals.permute(0, 2, 1)
-            out, indices, cmt_loss = model(signals)
-            rec_loss = (out - signals).abs().mean()
+            with autocast():
+                out, indices, cmt_loss = model(signals)
+                rec_loss = (out - signals).abs().mean()
             total_loss += rec_loss.item()
     model.train()
+    logging.debug("Evaluation completed.")
     return total_loss / len(data_loader)
 
 def train(model, train_loader, test_loader, optimizer, num_codes, checkpoint_dir, train_iterations=1000, alpha=1):
@@ -84,6 +93,8 @@ def train(model, train_loader, test_loader, optimizer, num_codes, checkpoint_dir
         (rec_loss + alpha * cmt_loss.mean()).backward()
 
         optimizer.step()
+        torch.cuda.empty_cache()
+
         pbar.set_description(
             f"rec loss: {rec_loss.item():.3f} | "
             + f"cmt loss: {cmt_loss.mean().item():.3f} | "
@@ -95,13 +106,17 @@ def train(model, train_loader, test_loader, optimizer, num_codes, checkpoint_dir
             "cmt_loss": cmt_loss.mean().item(),
             "active_percentage": indices.unique().numel() / num_codes * 100
         })
-
-        if (_ + 1) % 100 == 0:
-            test_loss = evaluate(model, test_loader)
-            wandb.log({"test_loss": test_loss})
-            save_checkpoint(model, optimizer, _ + 1, checkpoint_dir)
+        
+        try:
+            if (_ + 1) % 100 == 0:
+                test_loss = evaluate(model, test_loader)
+                wandb.log({"test_loss": test_loss})
+                save_checkpoint(model, optimizer, _ + 1, checkpoint_dir)
+                model.train()
+        except Exception as e:
+            print(f"An error occurred during evaluation: {e}")
             model.train()
-    return
+            return
 
 def main():
 
@@ -112,10 +127,10 @@ def main():
 
     csv_file = config["dataset"]["csv_file"]
     dataset_mimic_train = ECGDataset(csv_file=csv_file, split='train')
-    train_loader = DataLoader(dataset_mimic_train, batch_size=512, shuffle=True, num_workers=4)
+    train_loader = DataLoader(dataset_mimic_train, batch_size=config["training"]["batch_size"], shuffle=True, num_workers=16)
 
     dataset_mimic_test = ECGDataset(csv_file=csv_file, split='test')
-    test_loader = DataLoader(dataset_mimic_test, batch_size=512, shuffle=False, num_workers=4)
+    test_loader = DataLoader(dataset_mimic_test, batch_size=config["training"]["batch_size"], shuffle=False, num_workers=16)
 
     lr = float(config["training"]["learning_rate"])
     train_iter = config["training"]["train_iterations"]
@@ -123,10 +138,18 @@ def main():
     seed = config["training"]["seed"]
     checkpoint_dir = f"/mnt/rbanerjee/checkpoints/{config['training']['experiment_name']}"
     torch.random.manual_seed(seed)
-    model = SimpleVQAutoEncoder(
+    model = ResVQAutoEncoder(
         timesteps=dataset_mimic_train.waveform_length,
-        codebook_size=num_codes
+        codebook_size=num_codes,
+        implicit_neural_codebook=True
     ).to(device)
+
+    if torch.cuda.device_count() > 1:
+        print(f"Using {torch.cuda.device_count()} GPUs")
+        model = nn.DataParallel(model)
+
+    # Move the model to GPU
+    model = model.to(device)
 
     opt = torch.optim.AdamW(model.parameters(), lr=lr)
     train(model, train_loader, test_loader, train_iterations=train_iter, optimizer=opt, num_codes=num_codes, checkpoint_dir=checkpoint_dir)
