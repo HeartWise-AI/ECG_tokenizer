c0bb5ff360d943ed28e9b1f3f499a045682743a6
Rohan Banerjee <banerjee.rohan98@gmail.com>
2025-02-01 23:24:18 +0000

    Adding standard metrics for training and evaluation

--- linear_probe.py
+++ linear_probe.py
@@ -8,9 +8,11 @@ from torchvision import datasets, transforms
 from torch.cuda.amp import autocast
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, average_precision_score
 import argparse
 import wandb
+from metrics import compute_metrics
+import pandas as pd
 
 from data.dataset import ECGDatasetLinearProbe
 from models.vqvae import VQVAE, SimpleVQAutoEncoder, ResVQAutoEncoder, CodebookClassifier
@@ -26,6 +28,19 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
+
+def print_class_distribution(loader, split_name):
+    total_samples = 0
+    positive_samples = 0
+    for batch in tqdm(loader):
+        labels = batch['labels_values']
+        total_samples += len(labels)
+        positive_samples += labels.sum().item()
+    
+    print(f"{split_name} set distribution:")
+    print(f"Positive samples: {positive_samples} ({(positive_samples/total_samples)*100:.2f}%)")
+    print(f"Negative samples: {total_samples-positive_samples} ({((total_samples-positive_samples)/total_samples)*100:.2f}%)")
+    print(f"Total samples: {total_samples}")
 
 
 class FocalLoss(nn.Module):
@@ -49,39 +64,46 @@ class FocalLoss(nn.Module):
         else:
             return F_loss
 
-def evaluate(data_loader, classifier, criterion):
+def evaluate(data_loader, classifier, criterion, labels):
 
     classifier.eval()
     total_loss = 0
     all_preds = []
     all_labels = []
+    predictions_list = []
+    labels_list = []
     
     with torch.no_grad():
         for batch in data_loader:
             embeddings = batch['embedding'].float()
-            embeddings = embeddings[-1].unsqueeze(0)
+            # embeddings = embeddings[-1].unsqueeze(0)
             embeddings = embeddings.permute(1, 0, 2, 3)
             embeddings = embeddings.to(device)
-            labels = batch['labels'].to(device)
+            labels_values = batch['labels_values'].to(device)
             predictions = classifier(embeddings)
-            loss = criterion(predictions, labels)
-            total_loss += loss.item()
+            loss = criterion(predictions, labels_values)
+            # total_loss += loss.item()
+            # pred_prob = torch.sigmoid(predictions)
+            # pred_vals = (pred_prob > 0.5).int().cpu()
+            # all_preds.extend(pred_vals)
+            # all_labels.extend(labels.int().cpu().detach().numpy())
             pred_prob = torch.sigmoid(predictions)
-            pred_vals = (pred_prob > 0.5).int().cpu()
-            all_preds.extend(pred_vals)
-            all_labels.extend(labels.int().cpu().detach().numpy())
-    
-    print(len(data_loader))
-    avg_loss = total_loss / len(data_loader)
-    all_preds = np.array(all_preds)
-    all_labels = np.array(all_labels)
-    accuracy = accuracy_score(all_labels, all_preds)
+            labels_np = labels_values.int().cpu().detach().numpy()
+            predictions_list.append(pred_prob.int().cpu())
+            labels_list.append(labels_np)
+
+    all_preds = np.vstack(predictions_list)
+    all_labels = np.vstack(labels_list)
+    df_preds = pd.DataFrame(all_preds, columns=labels)
+    df_gt = pd.DataFrame(all_labels, columns=labels)
+    metrics = compute_metrics(df_gt, df_preds)
+    print(f"Epoch {epoch + 1}, Validation Loss: {loss.item():.4f}")
     classifier.train()
     
-    return avg_loss, accuracy
+    return metrics
 
 
-def train(classifier, train_loader, val_loader, test_loader, optimizer, criterion, num_epochs):
+def train(classifier, train_loader, val_loader, test_loader, optimizer, criterion, num_epochs, labels):
     classifier.train()
 
     pbar = trange(num_epochs)
@@ -89,43 +111,51 @@ def train(classifier, train_loader, val_loader, test_loader, optimizer, criterio
         epoch_losses = []
         all_preds = []
         all_labels = []
+        predictions_list = []
+        labels_list = []
         for batch in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}"):
             embeddings = batch['embedding'].float().to(device)
-            labels = batch['labels'].to(device)
+            labels_values = batch['labels_values'].to(device)
 
-            embeddings = embeddings[-1].unsqueeze(0)
+            # embeddings = embeddings[-1].unsqueeze(0)
             embeddings = embeddings.permute(1, 0, 2, 3)
             embeddings = embeddings.to(device)
             predictions = classifier(embeddings)
-            loss = criterion(predictions, labels)
+            loss = criterion(predictions, labels_values)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             pred_prob = torch.sigmoid(predictions)
-            pred_vals = (pred_prob > 0.5).int().cpu()
-            all_preds.extend(pred_vals)
-            all_labels.extend(labels.int().cpu().detach().numpy())
+            labels_np = labels_values.int().cpu().detach().numpy()
+            predictions_list.append(pred_prob.int().cpu())
+            labels_list.append(labels_np)
 
-        all_preds = np.array(all_preds)
-        all_labels = np.array(all_labels)
-        train_accuracy = accuracy_score(all_labels, all_preds)
-        print(f"Epoch {epoch + 1}, Training Loss: {loss.item():.4f}, Training Accuracy: {train_accuracy * 100:.10f}")
+        all_preds = np.vstack(predictions_list)
+        all_labels = np.vstack(labels_list)
+        df_preds = pd.DataFrame(all_preds, columns=labels)
+        df_gt = pd.DataFrame(all_labels, columns=labels)
+        metrics = compute_metrics(df_gt, df_preds)
+        print(f"Epoch {epoch + 1}, Training Loss: {loss.item():.4f}")
+        print(f"Metrics: {metrics['Regular']}")
+
+        # wandb.log({
+        #     "classification_loss": loss.item(),
+        #     "training_accuracy": train_accuracy * 100,
+        #     "training_precision": train_precision * 100,
+        #     "training_recall": train_recall * 100,
+        #     "training_f1": train_f1 * 100
+        # })
 
         if (epoch + 1) % 5 == 0:
-            val_loss, val_accuracy = evaluate(val_loader, classifier, criterion)
-            print(f"Epoch {epoch + 1}, Validation Loss: {val_loss:.4f}, Validation Accuracy: {val_accuracy * 100:.4f}")
-            wandb.log({
-                "test_loss": val_loss,
-                "test_accuracy": val_accuracy * 100
-            })
-        
-        wandb.log({
-            "classification_loss": loss.item(),
-            "training_accuracy": train_accuracy * 100
-        })
-    return
+            val_metrics = evaluate(val_loader, classifier, criterion, labels)
+            print(f"Validation Metrics: {val_metrics['Regular']}")
+            # wandb.log({
+            #     "test_loss": val_loss,
+            #     "test_accuracy": val_accuracy * 100
+            # })
 
+    return
 
 def main():
     seed = config["training"]["seed"]
@@ -133,14 +163,15 @@ def main():
     wandb.init(project="ECG_tokenizer_linear_probing", entity="rohanbanerjee", name="test_run_one_codebook", config=config)
     parquet_file = config["dataset"]["parquet_file"]
     embedding_folder = config["evaluation"]["embedding_dir"]
-    num_classes = 1
+    num_classes = 77
     embedding_dim = 160
     prev_embedding_dim = 128
-    num_quantizers = 1
+    num_quantizers = 8
     num_epochs = 1000
 
     dataset_mimic_train = ECGDatasetLinearProbe(parquet_file=parquet_file, embedding_folder=embedding_folder, split='train')
     train_loader = DataLoader(dataset_mimic_train, batch_size=8, shuffle=True, num_workers=16, pin_memory=True, drop_last=True)
+    labels = dataset_mimic_train[0]['labels']
 
     dataset_mimic_val = ECGDatasetLinearProbe(parquet_file=parquet_file, embedding_folder=embedding_folder, split='val')
     val_loader = DataLoader(dataset_mimic_val, batch_size=8, shuffle=False, num_workers=16, pin_memory=True, drop_last=True)
@@ -151,9 +182,9 @@ def main():
     classifier = CodebookClassifier(num_classes=num_classes, num_quantizers=num_quantizers, prev_embedding_dim=prev_embedding_dim, embedding_dim=embedding_dim).to(device)
     optimizer = optim.Adam(classifier.parameters(), lr=1e-6, weight_decay=1e-4)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.1)
-    # criterion = FocalLoss(logits=True)
-    criterion = nn.BCEWithLogitsLoss()
-    train(classifier, train_loader, val_loader, test_loader, optimizer, criterion, num_epochs)
+    criterion = FocalLoss(logits=True)
+    # criterion = nn.BCEWithLogitsLoss()
+    train(classifier, train_loader, val_loader, test_loader, optimizer, criterion, num_epochs, labels)
     
 if __name__ == '__main__':
     main()
