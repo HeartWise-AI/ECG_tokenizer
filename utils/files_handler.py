a862d26a85ec5f57929a07fc1cef10cb38fc4894
Rohan Banerjee <banerjee.rohan98@gmail.com>
2025-02-07 16:58:07 +0000

    small bug fix in naming convention

--- utils/files_handler.py
+++ utils/files_handler.py
@@ -18,15 +18,8 @@ def generate_output_dir_name(args, run_id):
     """
     import time
 
-    current_time = time.strftime("%Y%m%d-%H%M%S")
-    batch_size = args.batch_size
-    lr = args.lr
-    tag = args.tag if args.tag else "default"
-    project = args.project if args.project else "default_project"
-
     model_dir = (
-        f"{tag}_b{batch_size}_l{num_layers}_d{hidden_dim}_lr{lr}_{current_time}_{run_id}"
+        f"run_{run_id}"
     )
 
-    dir_name = os.path.join(project, model_dir)
-    return dir_name
+    return model_dir
