712b377d4fe91f064efcee1ef8609a0493007c0e
JacquesDelfrate <jdelfrate504@gmail.com>
2025-02-14 03:49:07 +0500

    adjust layer opf embedding reducer

--- models/embedding_reducer.py
+++ models/embedding_reducer.py
@@ -1,7 +1,11 @@
 import torch.nn as nn
 
 class EmbeddingReducer(nn.Module):
-    def __init__(self, input_shape=(8, 128, 160), output_size=768):
+    def __init__(
+        self, 
+        input_shape: tuple[int, int, int] = (8, 128, 160), 
+        output_size: int = 768
+    ):
         super(EmbeddingReducer, self).__init__()
         self.conv_layers = nn.Sequential(
             nn.Conv2d(in_channels=input_shape[0], out_channels=32, kernel_size=3, stride=2, padding=1),  # -> (32, 64, 80)
