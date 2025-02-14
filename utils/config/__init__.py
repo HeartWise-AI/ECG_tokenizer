18090b678fec923e7e0bed96c0caee0bd6ffdec6
JacquesDelfrate <jdelfrate504@gmail.com>
2025-02-15 03:29:13 +0500

    add gp2 finetuning config to config init module file

--- utils/config/__init__.py
+++ utils/config/__init__.py
@@ -1,4 +1,5 @@
 from .heartwise_config import HeartWiseConfig
 from .linear_probing_config import LinearProbingConfig
+from .gpt2_finetuning_config import GPT2FinetuningConfig
 
-__all__ = ["HeartWiseConfig", "LinearProbingConfig"]
+__all__ = ["HeartWiseConfig", "LinearProbingConfig", "GPT2FinetuningConfig"]
