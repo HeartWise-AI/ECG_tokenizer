9dd16500fd6163ae36c59e5b0d60a705fa275446
JacquesDelfrate <jdelfrate504@gmail.com>
2025-03-24 23:30:36 +0000

    support multigpu inference

--- runners/bert_report_classifier_runner.py
+++ runners/bert_report_classifier_runner.py
@@ -3,6 +3,7 @@ import torch
 import pandas as pd
 from tqdm import tqdm
 from torch.utils.data import DataLoader
+from utils.ddp import DistributedUtils
 
 from utils.enums import RunMode
 from utils.registry import RunnerRegistry
@@ -64,9 +65,14 @@ class BertReportClassifierRunner:
     def inference(self):
         self.model.eval()
         
-        ground_truth_classes: list[torch.Tensor] = []
-        predicted_classes: list[torch.Tensor] = []
-        for batch in tqdm(self.val_dataloader, desc="Inference", total=len(self.val_dataloader), disable=not self.config.is_ref_device):            
+        ground_truth_classes: list = []
+        predicted_classes: list = []
+        for batch in tqdm(
+            self.val_dataloader, 
+            desc="Inference", 
+            total=len(self.val_dataloader), 
+            disable=not self.config.is_ref_device
+        ):
             # Get predicted logits
             predicted_logits: dict[str, torch.Tensor] = self._val_step(
                 **batch['encoded_predicted_report']
@@ -85,38 +91,57 @@ class BertReportClassifierRunner:
             
             # Create thresholds tensor - need to do it here because of the batch size
             current_batch_size: int = len(batch['encoded_reference_report']['input_ids'])
-            bert_thresholds_tensor = torch.zeros((current_batch_size, self.config.num_classes)).to(self.config.device)
+            bert_thresholds_tensor = torch.zeros(
+                (current_batch_size, self.config.num_classes)
+            ).to(self.config.device)
             for i, pattern in enumerate(ECG_PATTERNS):
                 bert_thresholds_tensor[:, i] = BERT_THRESHOLDS[pattern]['threshold']
             
             # Get reference classes
-            reference_classes: torch.Tensor = torch.where(reference_probs >= bert_thresholds_tensor, 1, 0)
+            reference_classes: torch.Tensor = torch.where(
+                reference_probs >= bert_thresholds_tensor, 1, 0
+            )
 
             # Store predicted and ground truth classes
             for i in range(current_batch_size):
                 predicted_classes.append(predicted_probs[i].detach().cpu().numpy())
                 ground_truth_classes.append(reference_classes[i].detach().cpu().numpy())
             
-        metrics = compute_metrics(
-            df_gt=pd.DataFrame(ground_truth_classes, columns=ECG_PATTERNS), 
-            df_pred=pd.DataFrame(predicted_classes, columns=ECG_PATTERNS)
-        )
-                
-        save_json(
-            data=metrics, 
-            path=os.path.join(
-                self.config.output_folder, 
-                f'{self.config.pipeline_name}.json'
-            )
-        )
+        # Gather and aggregate results from all GPUs
+        results = {
+            "predicted_classes": predicted_classes,
+            "ground_truth_classes": ground_truth_classes,
+        }
+        gathered_results = [None for _ in range(self.config.world_size)]
+        DistributedUtils.all_gather_object(gathered_results, results)
         
-        save_to_csv(
-            metrics=metrics, 
-            path=os.path.join(
-                self.config.output_folder, 
-                f'{self.config.pipeline_name}.csv'
+        if self.config.is_ref_device:
+            combined_predicted_classes = []
+            combined_ground_truth_classes = []
+            for res in gathered_results:
+                combined_predicted_classes.extend(res["predicted_classes"])
+                combined_ground_truth_classes.extend(res["ground_truth_classes"])
+            
+            metrics = compute_metrics(
+                df_gt=pd.DataFrame(combined_ground_truth_classes, columns=ECG_PATTERNS), 
+                df_pred=pd.DataFrame(combined_predicted_classes, columns=ECG_PATTERNS)
             )
-        )
+                
+            save_json(
+                data=metrics, 
+                path=os.path.join(
+                    self.config.output_folder, 
+                    f'{self.config.pipeline_project}.json'
+                )
+            )
+            
+            save_to_csv(
+                metrics=metrics, 
+                path=os.path.join(
+                    self.config.output_folder, 
+                    f'{self.config.pipeline_project}.csv'
+                )
+            )
             
     def validate(self):
         raise NotImplementedError("validate not implemented")
