b8b42e7fa57d02826d633224f2f51e05b1887f2e
JacquesDelfrate <jdelfrate504@gmail.com>
2025-03-24 14:12:11 +0000

    add bert_report_classifier_project to projects module

--- projects/__init__.py
+++ projects/__init__.py
@@ -1,3 +1,4 @@
 from .llm_finetuning_project import LLMFinetuningProject
+from .bert_report_classifier_project import BertReportClassifierProject
 
-__all__ = ["LLMFinetuningProject"]
+__all__ = ["LLMFinetuningProject", "BertReportClassifierProject"]
