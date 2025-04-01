3a67aa83b1a1a1233f30abf0ecd2e90812dab561
JacquesDelfrate <jdelfrate504@gmail.com>
2025-04-01 16:27:29 +0000

    add ECGToekenizeTrainingProject to projects module

--- projects/__init__.py
+++ projects/__init__.py
@@ -1,4 +1,9 @@
 from .llm_finetuning_project import LLMFinetuningProject
+from .tokenizer_project import ECGTokenizerTrainingProject
 from .bert_report_classifier_project import BertReportClassifierProject
 
-__all__ = ["LLMFinetuningProject", "BertReportClassifierProject"]
+__all__ = [
+    "LLMFinetuningProject", 
+    "BertReportClassifierProject", 
+    "ECGTokenizerTrainingProject"
+]
