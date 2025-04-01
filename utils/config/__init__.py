81064f3f751ccbdb2e3691c045663719809911d5
JacquesDelfrate <jdelfrate504@gmail.com>
2025-04-01 16:28:52 +0000

    implement config pipeline fo ecg tokenizer training

--- utils/config/__init__.py
+++ utils/config/__init__.py
@@ -2,10 +2,12 @@ from .heartwise_config import HeartWiseConfig
 from .linear_probing_config import LinearProbingConfig
 from .llm_finetuning_config import LLMFinetuningConfig
 from .bert_classifier_config import BertReportClassifierConfig
+from .tokenizer_config import ECGTokenizerTrainingConfig
 
 __all__ = [
     "HeartWiseConfig", 
     "LinearProbingConfig", 
     "LLMFinetuningConfig", 
-    "BertReportClassifierConfig "
+    "BertReportClassifierConfig",
+    "ECGTokenizerTrainingConfig"
 ]
