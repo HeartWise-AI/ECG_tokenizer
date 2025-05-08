5c05faa701ce7c444251a62bcaa903cbc3f440be
Rohan Banerjee <banerjee.rohan98@gmail.com>
2025-05-08 00:15:11 +0000

    removed signal-wise normalization

--- data/dataset.py
+++ data/dataset.py
@@ -127,10 +127,8 @@ class ECGDataset(Dataset):
             if signal_range == 0:
                 print(f"Skipping {self.data_frame.iloc[idx]['waveform_path']}: signal has no variation (min={signal_min}, max={signal_max})")
                 return self.__getitem__((idx + 1) % len(self))
-            
-            signal: np.ndarray = (unnormalized_signal - signal_min) / signal_range * 2 - 1
 
-            return {'signal': np.transpose(signal, (1, 0))}
+            return {'signal': np.transpose(unnormalized_signal, (1, 0))}
         except Exception as e:
             print(f"Error processing index {self.data_frame.iloc[idx]['waveform_path']}: {str(e)}")
             return self.__getitem__((idx + 1) % len(self))
