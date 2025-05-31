f2cbdd9bf26e58f95695d4a9fab6649600c30679
JacquesDelfrate <jdelfrate@gmail.com>
2025-05-31 23:23:35 +0000

    update also models folder with __init__.py for new adapter structure

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from models.models import ResVQAutoEncoder
-from models.linear_reducer import LinearReducer
+from models.adapters import (
+    LinearReducer, 
+    EmbeddingReducer, 
+    SimpleEmbeddingReducer
+)
 from models.bert_classifier import BertClassifier
-from models.embedding_reducer import EmbeddingReducer
 from models.gpt2_with_embeddings import GPT2WithEmbedding
-from models.simple_embedding_reducer import SimpleEmbeddingReducer
 from models.tokenizer import (
     ECG_Tokenizer_Wrapper,
     ECG_CodebookClassifier,
