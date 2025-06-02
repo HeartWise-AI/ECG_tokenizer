c28b2b67f9e9dcf01bbade86269e8e41d3ace736
JacquesDelfrate <jdelfrate@gmail.com>
2025-06-02 19:14:44 +0000

    refactor code in ecg_dataset.py to get one fonction to load_ecg_signal -> can be shared to other dataloaders

--- data/ecg_dataset.py
+++ data/ecg_dataset.py
@@ -40,52 +40,55 @@ class ECGDataset(Dataset):
         return len(self.data)
         
     def load_ecg_signal(self, waveform_path: str) -> np.ndarray:
-        return np.load(waveform_path)
-    
-    def load_ecg_embedding(self, waveform_path: str) -> np.ndarray:
-        return np.load(waveform_path)
-    
-    def __getitem__(self, idx):
+        try:
+            waveform: np.ndarray = np.load(waveform_path)
+        except Exception as e:
+            print(f"Error loading ECG signal: {e}")
+            raise Exception(f"Error loading ECG signal: {e}")
         
+        # Hack for MHI dataset stored as 3D array with shape (2500, 12, 1)
+        if len(waveform.shape) == 3:
+            waveform = waveform.squeeze(-1)
+            
+        assert len(waveform.shape) == 2, f"Unnormalized signal has shape {waveform.shape}"
+        
+        return waveform
+
+    def __getitem__(self, idx):
         try:
             if not os.path.exists(self.data.iloc[idx]['waveform_path_psa']):
                 return self.__getitem__((idx + 1) % len(self))
 
-            unnormalized_signal: np.ndarray = self.load_ecg_signal(waveform_path=self.data.iloc[idx]['waveform_path_psa'])
+            waveform: np.ndarray = self.load_ecg_signal(
+                waveform_path=self.data.iloc[idx]['waveform_path_psa']
+            )
             
-            # Hack for MHI dataset stored as 3D array with shape (2500, 12, 1)
-            if len(unnormalized_signal.shape) == 3:
-                unnormalized_signal = unnormalized_signal.squeeze(-1)
-            
-            assert len(unnormalized_signal.shape) == 2, f"Unnormalized signal has shape {unnormalized_signal.shape}"
-            
-            if np.isnan(unnormalized_signal).any():
+            if np.isnan(waveform).any():
                 return self.__getitem__((idx + 1) % len(self))
             
-            current_length: int = unnormalized_signal.shape[0]
+            current_length: int = waveform.shape[0]
             if current_length > self.expected_waveform_length:
-                step: int = unnormalized_signal.shape[0] // self.expected_waveform_length
-                unnormalized_signal = unnormalized_signal[::step, :]
+                step: int = waveform.shape[0] // self.expected_waveform_length
+                waveform = waveform[::step, :]
             
-            if unnormalized_signal.shape[0] != self.expected_waveform_length:
+            if waveform.shape[0] != self.expected_waveform_length:
                 return self.__getitem__((idx + 1) % len(self))
             
-            if unnormalized_signal.shape[1] != self.num_leads:
+            if waveform.shape[1] != self.num_leads:
                 return self.__getitem__((idx + 1) % len(self))
-            
             
             # Only normalize if flag is set and we have lead statistics
             try:
                 if self.normalize_waveforms and self.lead_stats is not None:
-                    signal = np.zeros_like(unnormalized_signal)
+                    signal = np.zeros_like(waveform)
                     # Normalize each lead separately using its statistics
                     for lead_name, lead_idx in lead_to_idx.items():
                         mean = self.lead_stats[lead_name]["mean"]
                         std = self.lead_stats[lead_name]["std"]
-                        signal[:, lead_idx] = (unnormalized_signal[:, lead_idx] - mean) / std
+                        signal[:, lead_idx] = (waveform[:, lead_idx] - mean) / std
                         
                 else:
-                    signal = unnormalized_signal
+                    signal = waveform
             except Exception as e:
                 print(f"Error normalizing signal: {e}")
                 raise Exception(f"Error normalizing signal: {e}")
