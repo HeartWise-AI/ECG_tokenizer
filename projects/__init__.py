32e06d42b19dc9f4b8c8c08919b88026703ab659
JacquesDelfrate <jdelfrate504@gmail.com>
2025-06-11 11:32:37 +0000

    working linear probing pipeline

--- projects/__init__.py
+++ projects/__init__.py
@@ -1,9 +1,11 @@
 from .llm_finetuning_project import LLMFinetuningProject
 from .tokenizer_project import ECGTokenizerTrainingProject
+from .linear_probing_project import ECGTokenizerLinearProbing
 from .bert_report_classifier_project import BertReportClassifierProject
 
 __all__ = [
     "LLMFinetuningProject", 
+    "ECGTokenizerLinearProbing",
     "BertReportClassifierProject", 
-    "ECGTokenizerTrainingProject"
+    "ECGTokenizerTrainingProject",
 ]
