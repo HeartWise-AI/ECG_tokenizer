2f79df9db557c39cfe3fc4289b6112171f6cdc8e
Rohan Banerjee <banerjee.rohan98@gmail.com>
2025-06-16 14:54:16 +0000

    Revert: Move LLM additions to feature branch for PR

--- projects/bert_report_classifier_project.py
+++ projects/bert_report_classifier_project.py
@@ -68,4 +68,3 @@ class BertReportClassifierProject(BaseProject):
     
     def _setup_training_objects(self)->dict[str, Any]:
         raise NotImplementedError("Training is not implemented for this project")
-
