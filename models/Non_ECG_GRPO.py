9116a8fa9982236e13159304be3a1319f4901cff
Sabia Irfan <sabiairfan111@gmail.com>
2025-07-06 00:09:43 +0000

    updated code

--- models/Non_ECG_GRPO.py
+++ models/Non_ECG_GRPO.py
@@ -1,4 +1,4 @@
-from peft import LoraConfig, get_peft_model
+from peft import LoraConfig, get_peft_model, PeftModel
 import bitsandbytes as bnb 
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,30 +8,71 @@ from transformers import AutoTokenizer, AdamW
 import os 
 import torch
 from typing import Optional
+import re
 
 def model_4bit(model):
-
     for name, module in model.named_modules():
+        
         if isinstance(module, nn.Linear):
+            # Create a new Linear4bit layer with the same config as the original
+            quantized_layer = bnb.nn.Linear4bit(
+            module.in_features,
+            module.out_features,
+            bias=(module.bias is not None),
+            quant_type='nf4',
+            compute_dtype=torch.float16
+)
 
-            quantized_layer = bnb.nn.Linear4bit.from_linear(
-              module,
-              quant_type='nf4',
-              compute_dtype=torch.float16
-)            
+            # Copy weights and bias from the original linear layer
+            quantized_layer.weight.data.copy_(module.weight.data)
+            
+            if module.bias is not None:
+                
+                quantized_layer.bias.data.copy_(module.bias.data)
+            
+            # Now replace the original linear layer in the model with quantized one
             parent = model
-
+            
             parts = name.split(".")
-
+            
             for part in parts[:-1]:
-
-                #same as parent.part
+                
                 parent = getattr(parent, part)
-
-            #parent now the container for the layer to replace 
+            
             setattr(parent, parts[-1], quantized_layer)
     
-    return model 
+    return model
+
+def expand_final_answers(final_answers_list, num_candidates):
+
+    expanded_final_answers = []
+
+    for x in final_answers_list:
+
+        for y in range(num_candidates):
+
+            expanded_final_answers.append(x)
+    
+    return expanded_final_answers
+
+
+#from the outputted generated text extract the final answer
+#needed to compare with the actual final answer in the rewards function
+import re
+
+def extract_final_answer(text_list):
+    final_answers = []
+    for block in text_list:
+        # Find the first occurrence of A: number (e.g., A: 36 or A: 36s)
+        match = re.search(r"A:\s*([$€]?[0-9]+(?:\.[0-9]+)?)(s)?", block)
+        if match:
+            final_answers.append(match.group(1))
+        else:
+            # fallback: first number in the block
+            fallback = re.search(r"[$€]?[0-9]+(?:\.[0-9]+)?", block)
+            final_answers.append(fallback.group(0) if fallback else "NO_ANSWER")
+    return final_answers
+
 
 class Training:
 
@@ -61,7 +102,7 @@ class Training:
             self.model = get_peft_model(self.model, LoraConfig(**lora_config))
         
         if learning_rate == None:            
-            learning_rate = 2e-4
+            learning_rate = 1e-5
        
         self.learning_rate = learning_rate
 
@@ -104,7 +145,7 @@ class Training:
 
         #cost function
         #expects number of samples, number of classes (vocab size)
-        criterion = nn.CrossEntropyLoss()
+        criterion = nn.CrossEntropyLoss(ignore_index=-100)
 
         for epoch in range(num_epochs):
             
@@ -122,7 +163,7 @@ class Training:
                     attention_mask = attention_mask.to(self.device)
                
                 #forward pass
-                outputs = self.model.forward_ecg(input_ids = input_ids,
+                outputs = self.model(input_ids = input_ids,
                                                 labels = labels,
                                                 attention_mask = attention_mask,
                                                 return_dict = True)
@@ -143,6 +184,13 @@ class Training:
 
                 #back propogation
                 #accumulating the gradients
+
+
+                #print(f"Step {step} logits stats: min={logits.min().item()}, max={logits.max().item()}, mean={logits.mean().item()}")
+                #print(f"Step {step} labels valid count: {(labels != -100).sum().item()}")
+                #print(f"Step {step} loss: {loss.item()}")
+
+
                 loss.backward()
                 
                 #returns tensor number
@@ -152,12 +200,15 @@ class Training:
                 #force optimization step at the last batch
                 if (step + 1) % gradient_accumulation_steps == 0 or (step + 1) == len(dataloader):
                     #optimizing step
+                  
+                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+                  
                     self.optimizer.step()
                     self.optimizer.zero_grad()
             
             avg_loss = total_loss /len(dataloader)
 
-            print(f"Epoch {epoch + 1} average loss: {avg_loss:.4f}")
+            print(f"Training adapters: \n Epoch {epoch + 1} average loss: {avg_loss:.4f}")
     
     def save_adapter_checkpoint(self, path):
 
@@ -169,14 +220,14 @@ class Training:
         print(f"Adapter saved at path {path}")
 
 
-    def generate_initial_reports(self, dataloader, base_model, adapter_path, max_new_tokens = 128):
+    def generate_initial_reports(self, dataloader, adapter_path, max_new_tokens = 128):
 
         #input saved adapters in the file into base model
-        adapter_model = PeftModel.from_pretrained(base_model, adapter_path)
+        self.model.load_adapter(adapter_path, adapter_name="lora_adapter")
 
-        adapter_model.to(self.device)
+        self.model.to(self.device)
 
-        adapter_model.eval()
+        self.model.eval()
 
         #the list containing all the outputs generated 
         all_reports = []
@@ -195,18 +246,21 @@ class Training:
 
                 #beams are deterministic 
                 #generates token ID sequences
-                generated_ids = adapter_model.generate(
+                generated_ids = self.model.generate(
                     input_ids = input_ids,
                     attention_mask = attention_mask,
                     max_new_tokens = max_new_tokens, #number of tokens that can be generated after input seq length
                     do_sample = False,
                     num_beams = 4,
-                    num_return_sequences = 1
+                    num_return_sequences = 1,
+                    temperature=0
                 )
-
+               
             #converts each sequence of tokens into string
             #a list
             reports = self.text_tokenizer.batch_decode(generated_ids, skip_special_tokens = True)
+
+            print(reports)
 
             #adding the text generated from this batch 
             all_reports.extend(reports)
@@ -214,13 +268,13 @@ class Training:
         return all_reports
 
 
-    def grpo(self, dataloader, adapter_path, epochs = 4, max_new_tokens = 128, num_candidates = 4):
+    def grpo(self, final_answer_list, dataloader, adapter_path, epochs = 4, max_new_tokens = 10, num_candidates = 2):
 
-        adapter_model = PeftModel.from_pretrained(base_model, adapter_path)
+        self.model.load_adapter(adapter_path, adapter_name="lora_adapter")
 
-        adapter_model.to(self.device)
+        self.model.to(self.device)
 
-        adapter_model.train()
+        self.model.train()
 
         for epoch in range(epochs):
 
@@ -240,53 +294,50 @@ class Training:
 
                   attention_mask = attention_mask.to(self.device)
 
-                for x in range(num_candidates):
-
-                    #using do sample for more variability 
-                    generated_ids = adapter_model.generate(
+                #using do sample for more variability 
+                generated_ids = self.model.generate(
                         input_ids = input_ids,
                         attention_mask = attention_mask,
                         max_new_tokens = max_new_tokens,
-                        do_sample = True,
-                        top_k = 50, #only sample from the top 50 most likely
-                        top_p = 0.95, #sample from smallest set of possible tokens whose prob above 95%
-                        num_return_sequences = 1 #generate 1 output sequence per input 
+                        do_sample = False,
+                        num_beams=4,
+                        num_return_sequences = num_candidates #generate num candidates output sequence per input 
                     ) #batch_size * num_return_sequences, sequence_length + max_new_tokens
-
-                    #a list
-                    candidates = self.text_tokenizer.batch_decode(generated_ids, skip_special_tokens = True)
-
-                    outputs_for_batch.extend(candidates)
+                
+                #decode the outputs for the batch
+                #list of stringss
+                outputs_for_batch = self.text_tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+                print(outputs_for_batch[0])
 
                 input_batch_size = input_ids.shape[0] #batch size, seq length
+                #print(input_batch_size)
 
-                final_scores = extract_final_answer(outputs_for_batch)
+                #generated final answers
+                final_answers = extract_final_answer(outputs_for_batch)
+                #print(final_answers)
+                #print(len(final_answers))
+
+                expanded_final_answers_list = expand_final_answers(final_answer_list, num_candidates)
+                #print(expanded_final_answers_list)
 
                 #return list of scores
-                scores = self.reward_model(outputs_for_batch, final_scores)
+                scores = self.reward_model(final_answers, final_answer_list)
+                #print(scores)
 
                 scores_tensor = torch.tensor(scores, device = self.device)
+                #print(scores_tensor)
 
                 #each row in each batch is an input, each column is output generated for that input
                 scores_tensor = scores_tensor.view(input_batch_size, num_candidates)
 
-                avg_score_list = []
+                #shape batch size, 1 
+                #dim = 1 takes the average across second dim num_candidates
+                avg_scores = scores_tensor.mean(dim=1, keepdim=True)
 
-                for x in range(input_batch_size):
-
-                    #Average score across all candidates for each input in the batch 
-                    avg_score_list.append(scores_tensor[x].mean())
-                
                 #batch size, num_candidates
-                advantage_tensor = torch.empty_like(scores_tensor)
+                advantage_tensor = scores_tensor - avg_scores
 
-                for i in range(input_batch_size):
-
-                    for j in range(num_candidates):
-
-                        advantage_tensor[i][j] = scores_tensor[i][j] - avg_score_list[i]
-
-                for i, candidate in enumerate(outputs_for_batch):
+                for candidate in outputs_for_batch:
 
                     #tokenzie the output 
                     tokens = self.text_tokenizer(candidate, return_tensors = "pt").to(self.device)
@@ -294,63 +345,56 @@ class Training:
                     input_ids_c = tokens["input_ids"]
                     attention_mask_c = tokens["attention_mask"]
 
-                    with torch.no_grad():
-
-                        output = adapter_model(input_ids = input_ids_c,
+                    output = self.model(input_ids = input_ids_c,
                                                 attention_mask = attention_mask_c
                                                 #need the ecg signals for the specific batch that the candidate was produced from
                                                 #unsqueeze to make it back to (1, channels, seq length)
                                                 )
                         
-                        logits = output.logits #shape [1, seq len, vocab_size]
+                    logits = output.logits #shape [1, seq len, vocab_size]
 
-                        #apply softmax on last dimension
-                        #log probabilities for all vocab tokens at every position vocab_size
-                        log_probs = F.log_softmax(logits, dim=-1)
+                    #apply softmax on last dimension
+                    #log probabilities for all vocab tokens at every position vocab_size
+                    log_probs = F.log_softmax(logits, dim=-1)
 
-                        #drop the prediction at the last token, since last token nothing to predict afterwards
-                        shifted_log_probs = log_probs[:, :-1, :]
+                    #drop the prediction at the last token, since last token nothing to predict afterwards
+                    shifted_log_probs = log_probs[:, :-1, :]
 
-                        #drop the BOS token
-                        shifted_input_ids = input_ids_c[:,1:] #batch, seq length
+                    #drop the BOS token
+                    shifted_input_ids = input_ids_c[:,1:] #batch, seq length
 
-                        #add third dimension to match shifted_log_probs
-                        shifted_input_ids = shifted_input_ids.unsqueeze(-1)
+                    #add third dimension to match shifted_log_probs
+                    shifted_input_ids = shifted_input_ids.unsqueeze(-1)
 
-                        #Extracts the log-probability of the correct token at each position
-                        token_log_probs = shifted_log_probs.gather(2, shifted_input_ids) #[batch_size, seq_len, 1]
+                    #Extracts the log-probability of the correct token at each position
+                    token_log_probs = shifted_log_probs.gather(2, shifted_input_ids) #[batch_size, seq_len, 1]
 
-                        #remove the last dimension
-                        #batch size = 1
-                        token_log_probs = token_log_probs.squeeze(-1) #[batch_size, seq_len]
+                    #remove the last dimension
+                    #batch size = 1
+                    token_log_probs = token_log_probs.squeeze(-1) #[batch_size, seq_len]
 
-                        log_prob_sum = 0.0
+                    #takes the sum of the token logs since batch size is 1
+                    log_prob_sum = token_log_probs.sum()
 
-                        token_log_seq_length = token_log_probs.shape[-1]
+                    #add to log probs list
+                    log_probs_list.append(log_prob_sum)
 
-                        #for every output in the output batch list
-                        for j in range(token_log_seq_length):
-
-                            log_prob_sum += token_log_probs[0][j].item()
-                        
-                        log_probs_list.append(log_prob_sum)
-
-            log_probs_tensor = torch.tensor(log_probs_list, device = self.device)
+                log_probs_tensor = torch.stack(log_probs_list)
             
-            log_probs_tensor = log_probs_tensor.view(input_batch_size, num_candidates)
+                log_probs_tensor = log_probs_tensor.view(input_batch_size, num_candidates)
 
-            #compute GRPO loss
-            loss = -(log_probs_tensor * advantage_tensor).mean()
+                #compute GRPO loss
+                loss = -(log_probs_tensor * advantage_tensor).mean()
 
-            self.optimizer.zero_grad()
+                self.optimizer.zero_grad()
 
-            loss.backward()
+                loss.backward()
 
-            self.optimizer.step()
+                self.optimizer.step()
 
-            batch_number += 1 
+                batch_number += 1 
 
-            print(f"Epoch {epoch} | Batch number {batch_number} | Loss : {loss.item():.4f}")
+                print(f"GRPO: \n Epoch {epoch} | Batch number {batch_number} | Loss : {loss.item():.4f}")
 
 
 
--- models/Test_GRPO.py
+++ models/Test_GRPO.py
@@ -6,6 +6,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from torch.utils.data import Dataset, DataLoader
 from Non_ECG_GRPO import Training, model_4bit
 from peft import LoraConfig, get_peft_model, TaskType
+import bitsandbytes as bnb 
+from sklearn.model_selection import train_test_split
 
 # Downlad latest version
 path = kagglehub.dataset_download("thedevastator/grade-school-math-8k-q-a")
@@ -40,12 +42,28 @@ def parse_follow_up_questions(text):
 
       parts.append(p.strip())
 
-    questions = parts[0::2]
+    answers = parts[0::2]
 
-    return questions
+    questions = []
+
+    cleaned_questions = []
+
+    for x in answers:
+      if "?" in x:
+        questions.append(x)
+
+
+    for y in questions:
+      if "\n" not in y:
+        cleaned_questions.append(y)
+      else:
+        cleaned_questions.append(y.split("\n")[-1])
+  
+
+    return cleaned_questions
   
 df["follow_up_questions"] = df["answer"].apply(parse_follow_up_questions)
-#print(df["follow_up_questions"])
+#print(df["follow_up_questions"][0])
 
 #make follow up answers column (list of the follow up answers to the follow up questions)
 def parse_follow_up_answers(text):
@@ -60,59 +78,47 @@ def parse_follow_up_answers(text):
 
     answers = parts[1::2]
 
-    return answers
+    clean = []
+
+    for x in answers:
+      
+      text = x.split(">>")[-1]
+      
+      text = text.split("\n")[0]
+
+      clean.append(text)
+
+    return clean
   
 df["follow_up_answers"] = df["answer"].apply(parse_follow_up_answers)
 #print(df["follow_up_answers"])
 #print(df.head())
 
 #prompt engineering 
-def creating_prompts(df):
+
+#question + final answer
+def full_text(df):
   inputs = []
 
   for index, row in df.iterrows():
-    prompt = "Q: " + str(row["question"]).strip() + "\nA:" + str(row["final_answers"]).strip()
+    #needs to generate the final answer
+    prompt = "Answer the following question with only the final answer, no explanation or additional text. Q: " + str(row["question"]).strip() + "\n A: " + str(row["final_answers"]).strip()
     inputs.append(prompt)
 
   return inputs
 
-#prepare inputs for batching
-#each item in the batch should include input ids, labels, attention_mask
-class TextDataset(Dataset):
+#only the prompt
+def prompt_only(df):
+  inputs = []
 
-    def __init__(self, tokenized_inputs):
-        self.input_ids = tokenized_inputs['input_ids']
-        self.labels = tokenized_inputs['input_ids'].clone()
-        
-        self.attention_mask = tokenized_inputs.get('attention_mask', None)
+  for index, row in df.iterrows():
+    #needs to generate the final answer
+    prompt = "Answer the following question with only the final answer, no explanation or additional text. Q: " + str(row["question"]).strip() + "\n A: " 
+    inputs.append(prompt)
 
-    #for Cross entropy padded tokens need labels of -100 to not be included in the calculations
-        if self.attention_mask is not None:
-            for i in range(self.attention_mask.shape[0]):       # batch dimension
-                for j in range(self.attention_mask.shape[1]):   # sequence length dimension
-                    if self.attention_mask[i, j] == 0:
-                        self.labels[i, j] = -100
-
-    #batch size
-    def __len__(self):
-        return self.input_ids.shape[0]
+  return inputs
 
 
-    #returns item in the batch as a dictionary given it's index
-    def __getitem__(self, index):
-    
-        item = {
-        "input_ids": self.input_ids[index],
-        "labels": self.labels[index]
-            }
-        
-        if self.attention_mask is not None:
-            item["attention_mask"] = self.attention_mask[index]
-        
-        return item
-
-#from the outputted generated text extract the final answer
-#needed to compare with the actual final answer in the rewards function
 def extract_final_answer(generated_text_list):
 
     final_answer_list = []
@@ -123,13 +129,91 @@ def extract_final_answer(generated_text_list):
 
     return final_answer_list
 
+#calculate prompt lengths
+#not all prompts have the same length
+def calculate_prompt_length(prompts, tokenizer, max_length):
+
+  prompt_lengths = []
+
+  for prompt_text in prompts:
+        # Tokenize individual prompt without padding
+        tokens = tokenizer(
+            prompt_text,
+            truncation=True,
+            max_length=max_length,
+            return_tensors="pt"
+        )
+        
+        # Get actual length (excluding padding)
+        #sequence length
+        actual_length = tokens["input_ids"].shape[1]
+        prompt_lengths.append(actual_length)
+  
+  return prompt_lengths
+
+
+#prepare inputs for batching
+#each item in the batch should include input ids, labels, attention_mask
+class TextDataset(Dataset):
+
+    def __init__(self, full_tokenized_inputs, prompt_lengths):
+        
+        #only take up to the question, not the final answer of the full text
+        self.input_ids = full_tokenized_inputs["input_ids"]
+
+        #batch size, seq length
+        labels = full_tokenized_inputs['input_ids'].clone()
+    
+        self.attention_mask = full_tokenized_inputs.get('attention_mask', None)
+
+        #list of the prompt lengths
+        self.prompt_lengths = prompt_lengths
+
+        #mask the previous text, only final answer used in calculations
+        #all rows, all columns except the last 
+        #everything but the final answer should be -100
+        for i in range(labels.shape[0]):
+            
+            labels[i, :prompt_lengths[i]] = -100
+
+        if self.attention_mask is not None:
+        # mask padding tokens, they should not be considered in loss calculation
+          #padding_mask = (self.attention_mask == 0)
+          #labels[padding_mask] = -100
+
+          for i in range(self.attention_mask.shape[0]):
+            for j in range(self.attention_mask.shape[1]):
+              if self.attention_mask[i, j] == 0:
+                labels[i, j] = -100
+        
+        self.labels = labels
+
+
+    #batch size
+    def __len__(self):
+        return self.input_ids.shape[0]
+
+
+   #returns item in the batch as a dictionary given it's index
+    def __getitem__(self, index):
+    
+        item = {
+        "input_ids": self.input_ids[index],
+        "labels": self.labels[index]           }
+        
+        if self.attention_mask is not None:
+        
+            item["attention_mask"] = self.attention_mask[index]
+        
+        return item
+
 
 #rewards function gives the output a score of 1.0 if the final answer matches the actual final answer else 0.0 score
-def rewards_function(generated_text_list, final_answer_list):
+def rewards_function(generated_answer_list, final_answer_list):
     
     list_of_scores = []
 
-    for i, text in enumerate(generated_text_list):
+    for i, text in enumerate(generated_answer_list):
         outputted_answer = None
         answer = final_answer_list[i]
 
@@ -146,11 +230,29 @@ def rewards_function(generated_text_list, final_answer_list):
 
     return list_of_scores
 
-prompts = creating_prompts(df)
-#print(prompts)
+#split_index = int(len(df)*0.8)
 
-final_list = extract_final_answer(prompts)
-#print(final_list)
+#df_sft = df[:split_index]
+#df_rl = df[split_index:]
+
+df_sft, df_rl = train_test_split(df, test_size=0.2, random_state=42)
+
+df_sft = df_sft.sample(frac=0.1, random_state=42).reset_index(drop=True)
+
+df_rl = df_rl.sample(frac=0.1, random_state=42).reset_index(drop=True)
+
+final_answers_rl = df_rl["final_answers"].tolist()
+
+
+full_text_sft = full_text(df_sft)
+#print(full_text)
+
+full_text_rl = full_text(df_rl)
+
+prompt_sft = prompt_only(df_sft)
+#print(prompt)
+
+prompt_rl = prompt_only(df_rl)
 
 #loading the model
 model = AutoModelForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v0.3")
@@ -161,44 +263,81 @@ tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v0.3")
 #make the padding token the end of sequence token
 tokenizer.pad_token = tokenizer.eos_token
 
-#tokenizing the inputs
-tokenized_inputs = tokenizer(
-    prompts,
-    padding=True,
+MAX_LENGTH = 512
+
+#tokenizing the full text
+#max length so all of the sequences are the same length (pad to the max length, truncate to max length)
+tokenized_text = tokenizer(
+    full_text_sft,
+   padding=True,
     truncation=True,
-    return_tensors="pt"
-)
+    max_length = MAX_LENGTH,
+    return_tensors="pt")
+
+tokenized_prompt_sft = tokenizer(
+    prompt_sft,
+   padding=True,
+    truncation=True,
+    max_length = MAX_LENGTH,
+    return_tensors="pt")
+
+#print(tokenized_text["input_ids"])
+#decoded_text = tokenizer.decode(tokenized_text["input_ids"][0], skip_special_tokens=True)
+#print(decoded_text)
+
+#full_ids = tokenized_text["input_ids"][0]
+#print(len(full_ids))
+
+tokenized_text_rl = tokenizer(
+    full_text_rl,
+   padding=True,
+    truncation=True,
+    max_length = MAX_LENGTH,
+    return_tensors="pt")
+
+tokenized_prompt_rl = tokenizer(
+    prompt_rl,
+   padding=True,
+    truncation=True,
+    max_length = MAX_LENGTH,
+    return_tensors="pt")
+
+
+prompt_lengths_sft = calculate_prompt_length(prompt_sft, tokenizer, MAX_LENGTH)
+
+prompt_lengths_rl = calculate_prompt_length(prompt_rl, tokenizer, MAX_LENGTH)
 
 #turn the tokenized inputs into batches
-dataset = TextDataset(tokenized_inputs)
-#length = dataset.__len__()
+dataset_sft = TextDataset(tokenized_text, prompt_lengths_sft)
+
+dataset_rl = TextDataset(tokenized_text_rl, prompt_lengths_rl)
+
+length = dataset_sft.__len__()
 #print(length)
 
-#tenth = dataset.__getitem__(10)
+#tenth = dataset_sft.__getitem__(10)
 #print(tenth)
 
 #define how many batches to break up data into
+
 batch_size = 8
 #make into batches
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+dataloader_sft = DataLoader(dataset_sft, batch_size=batch_size, shuffle=True)
+
+dataloader_rl = DataLoader(dataset_rl, batch_size=batch_size, shuffle=True)
 
 #print(dataloader.__len__())
 
 #make model 4 bit
 quant_model = model_4bit(model)
-for name, module in quant_model.named_parameters():
-    print(name)
+#for name, module in quant_model.named_parameters():
+#  print(name)
 
-r = 16
+#r and lora_alpha same value
 
-lora_config = LoraConfig(
-    r = r,
-    lora_alpha = r,
-    target_modules = ["q_proj",  "v_proj"],
-    lora_dropout = 0.1,
-    bias = "none",
-    task_type=TaskType.CAUSAL_LM
-)
+lora_config = {
+
+  "r": 16, "lora_alpha": 16, "target_modules": ["q_proj", "v_proj"], "lora_dropout": 0.1, "bias": "none", "task_type":TaskType.CAUSAL_LM}
 
 #optim already default defined in training class 
 #learning rate default defined in training class
@@ -208,19 +347,26 @@ lora_config = LoraConfig(
 
 pipeline_model = Training(reward_model = rewards_function, model = quant_model, lora_config = lora_config)
 
-gipeline_model.train_adapters(dataloader, num_epochs=2, gradient_accumulation_steps=8)
+#for name, param in pipeline_model.model.named_parameters():
+#    if "lora" in name:
+#        print(name, param.data.mean().item())
+
+#
+pipeline_model.train_adapters(dataloader_sft, num_epochs=2, gradient_accumulation_steps=8)
 
 #making the path (does not exist yet)
 adapter_path = "/home/sirfan/ECG_tokenizer/models/adapter_weights"
 
 #save the weights at the path
-pipeline_model.save_adapter_checkpoint(adapter_path)
+#pipeline_model.save_adapter_checkpoint(adapter_path)
 
 #generate reports
-reports = pipeline_model.generate_initial_reports(dataloader, pipeline_model.model, adapter_path, max_new_tokens=128)
+#reports = pipeline_model.generate_initial_reports(dataloader_sft, adapter_path, max_new_tokens=128)
+#print(reports)
+print(len(dataloader_rl))
 
 #train the model
-pipeline_model.grpo(dataloader, adapter_path, epochs=4, max_new_tokens = 128, num_candidates=4)
+#pipeline_model.grpo(final_answers_rl, dataloader_rl, adapter_path, epochs=4, max_new_tokens = 128, num_candidates=4)
 
 
 
