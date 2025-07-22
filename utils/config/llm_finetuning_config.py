92f3939b749b80b3c34e68d857f73c2952d7c20e
Rohan Banerjee <banerjee.rohan98@gmail.com>
2025-07-22 19:50:28 +0000

    removed unused parameter

--- utils/config/llm_finetuning_config.py
+++ utils/config/llm_finetuning_config.py
@@ -40,7 +40,6 @@ class LLMFinetuningConfig(HeartWiseConfig):
 
     # Model parameters
     huggingface_model_name: str
-    trainable_model_name: str
     adapter_name: str
     llm_input_embedding_size: int
     
